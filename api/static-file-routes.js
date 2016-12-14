050100626d9229dcbe3fbb36e82c458302b5ff8e
Taylor Emolo <tayloremolo@tay.local>
2016-12-13 19:58:44 -0500

    fixed bug

--- api/static-file-routes.js
+++ api/static-file-routes.js
@@ -9,9 +9,9 @@ module.exports = function(app){
 	res.sendFile(path.join(__dirname, './../public/index.html'));
 	});
 
-	app.get('/profile', function(req,res) {
-		res.sendFile(path.join(__dirname, './../public/profile.html'));
-	});
+	// app.get('/profile', function(req,res) {
+	// 	res.sendFile(path.join(__dirname, './../public/profile.html'));
+	// });
 
 	app.get('/css/:name', function(req, res) {
 		var fileName = req.params.name;
